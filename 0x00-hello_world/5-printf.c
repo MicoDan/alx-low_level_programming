ad624a8e27a2f62c8ff989ed866f585b625a8a0a
Mico Dan <micodan369@gmail.com>
2023-02-16 17:50:40 +0200

    adkjf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  *
@@ -6,6 +7,7 @@
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,");
-return 0;
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
+
