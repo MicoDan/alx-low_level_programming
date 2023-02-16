47ad51780e9a76926767a2e4582ce848519e703b
Mico Dan <micodan369@gmail.com>
2023-02-16 18:01:43 +0200

    ameesa

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,7 +11,6 @@ printf("Size of an int: %ld byte(s)\n", sizeof(int));
 printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
 printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
 printf("Size of a float: %ld byte(s)\n", sizeof(float));
-printf("Size of a double: %ld byte(s)\n", sizeof(double));
 
 return (0);
 }
