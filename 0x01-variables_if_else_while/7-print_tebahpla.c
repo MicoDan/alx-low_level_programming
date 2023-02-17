f9c980d481764db86873e90c383ba841a7a9881a
Mico Dan <micodan369@gmail.com>
2023-02-17 21:46:15 +0200

    a message

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -6,15 +6,17 @@
  */
 int main(void)
 {
-int i;
+    int i;
 
-for (i = 'z'; i >= 'a'; i--)
-{
-putchar(i);
+    for (i = 'z'; i >= 'a'; i--)
+    {
+        putchar(i);
+    }
+
+    putchar('\n');
+
+    return 0;
 }
 
-putchar('\n');
 
-return (0);
-}
 
--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -6,16 +6,24 @@
  */
 int main(void)
 {
-int i;
+    int i;
 
-for (i = 'z'; i >= 'a'; i--)
-{
-putchar(i);
+    for (i = 0; i < 16; i++)
+    {
+        if (i < 10)
+        {
+            putchar(i + '0');
+        }
+        else
+        {
+            putchar(i - 10 + 'a');
+        }
+    }
+
+    putchar('\n');
+
+    return 0;
 }
 
-putchar('\n');
-
-return (0);
-}
 
 
