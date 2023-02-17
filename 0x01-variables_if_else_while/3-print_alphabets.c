931cc5c76b9a7c620f72d8c8923bf721ec283d06
Mico Dan <micodan369@gmail.com>
2023-02-17 17:26:49 +0200

    hope

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,19 +5,28 @@
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 char letter = 'a';
+int i;
 
-while (letter <= 'z')
+for (i = 0; i < 26; i++)
 {
 putchar(letter);
 letter++;
 }
-while (toupper(letter) == 'Z');
+
+letter = 'A';
+for (i = 0; i < 26; i++)
 {
 putchar(letter);
+letter++;
 }
+
+putchar('\n');
+
 return (0);
 }
+
 
