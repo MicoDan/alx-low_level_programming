5e059b8fc25a1d651b51eb244d1b7c5f2667b240
Mico Dan <micodan369@gmail.com>
2023-02-17 17:38:46 +0200

    a commit

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -5,23 +5,23 @@
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-char letter = 'a';
-int i;
+    char letter = 'a';
+    int i;
 
-for (i = 0; i < 26; i++)
-{
-if(i==5 || i==17){
-continue;
+    for (i = 0; i < 26; i++)
+    {
+        if (i == 5 || i == 17)
+        {
+            continue;
+        }
+        putchar(letter);
+        letter++;
+    }
+
+    putchar('\n');
+
+    return (0);
 }
-putchar(letter);
-letter++;
-}
 
-
-putchar('\n');
-
-return (0);
-}
