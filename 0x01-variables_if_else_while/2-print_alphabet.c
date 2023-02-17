3d2ee94fbc22eed4880ca1c2e02a4589aac5090e
Mico Dan <micodan369@gmail.com>
2023-02-17 17:48:09 +0200

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char letter = 'a';
+int i;
+
+for (i = 0; i < 26; i++)
+{
+putchar(letter);
+letter++;
+}
+
+putchar('\n');
+
+return (0);
+}
