d1facb948c2dec340672c2d5ba407ffb3183b3db
Mico Dan <micodan369@gmail.com>
2023-02-18 05:10:40 +0200

    commit

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -6,23 +6,23 @@
  */
 int main(void)
 {
-    int i;
+int i;
 
-    for (i = 0; i < 16; i++)
-    {
-        if (i < 10)
-        {
-            putchar(i + '0');
-        }
-        else
-        {
-            putchar(i - 10 + 'a');
-        }
-    }
+for (i = 0; i < 16; i++)
+{
+if (i < 10)
+{
+putchar(i + '0');
+}
+else
+{
+putchar(i - 10 + 'a');
+}
+}
 
-    putchar('\n');
+putchar('\n');
 
-    return 0;
+return 0;
 }
 
 
