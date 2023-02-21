77fd1a0e2d68c1b967437a361bd6ee5945490196
Mico Dan <micodan369@gmail.com>
2023-02-22 00:53:35 +0200

    2commits

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet - prints the alphabet in lowercase followed by a new line
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet(void)
+{
+char letter = 'a';
+
+while (letter <= 'z')
+{
+_putchar(letter);
+letter++;
+}
+
+_putchar('\n');
+}
