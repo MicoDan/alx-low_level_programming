60fc2875658977f7a19c1db650952445b93b406d
Mico Dan <micodan369@gmail.com>
2023-02-22 04:29:31 +0200

    task5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,17 +11,17 @@ int print_sign(int n)
 {
 if (n > 0)
 {
-putchar('+');
+putchar('+' + 1);
 return (1);
 }
 else if (n == 0)
 {
-putchar('0');
+putchar('0' + 0);
 return (0);
 }
 else
 {
-putchar('-');
+putchar('-' + 1);
 return (-1);
 }
 }
