a93ea2b350c21e758b34c3a8eb509be81c459a1e
Mico Dan <micodan369@gmail.com>
2023-02-24 05:24:50 +0200

    task

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * _isupper - checks for uppercase character
+ * @c: integer character to be checked
+ *
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+int _isupper(int c)
+{
+        /* Checks if the ASCII value of the character is between 65 and 90, inclusive */
+	if (c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
+}
