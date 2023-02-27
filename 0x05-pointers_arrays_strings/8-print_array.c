00785a0da1b5a3876f048006ee4cb05688ef0458
Mico Dan <micodan369@gmail.com>
2023-02-27 14:51:33 +0200

    task 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 /**
  * print_array - prints n elements of an array of integers
  * @a: pointer to the first element of the array
@@ -7,51 +8,18 @@
  */
 void print_array(int *a, int n)
 {
-int i, num, count;
+int i;
+
 if (a == NULL)
 return;
 
 for (i = 0; i < n; i++)
 {
-num = a[i];
-count = 0;
-
-/* Handle negative numbers */
-if (num < 0)
-{
-putchar('-');
-num = -num;
+printf("%d", a[i]);
+if (i != n - 1)
+printf(", ");
+}
+printf("\n");
 }
 
-/* Count number of digits in the number */
-if (num == 0)
-count = 1;
-else
-{
-while (num > 0)
-{
-count++;
-num /= 10;
-}
-}
-/* Print the number */
-num = a[i];
-if (num < 0)
-num = -num;
-do
-{
-putchar(num / pow(10, count - 1) + '0');
-num %= (int) pow(10, count - 1);
-count--;
-} while (count > 0);
-
-/* Print comma and space except for the last element */
-if (i < n - 1)
-{
-putchar(',');
-putchar(' ');
-}
-}
-putchar('\n');
-}
 
