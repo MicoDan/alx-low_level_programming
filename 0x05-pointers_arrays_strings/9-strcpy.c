d4be487d86aa0abe383ec5b84c43133f1c70e02b
Mico Dan <micodan369@gmail.com>
2023-02-27 16:27:55 +0200

    task 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,16 +9,16 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-  int i = 0;
+int i = 0;
 
-  while (src[i] != '\0')
-  {
-    dest[i] = src[i];
-    i++;
-  }
+while (src[i] != '\0')
+{
+dest[i] = src[i];
+i++;
+}
 
-  dest[i] = '\0';
+dest[i] = '\0';
 
-  return dest;
+return dest;
 }
 
