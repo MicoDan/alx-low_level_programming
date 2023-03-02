1f7689198ea927a9d8de34637baa818b319674e0
Mico Dan <micodan369@gmail.com>
2023-03-02 04:58:38 +0200

    task 3

---
+++ 0x06-pointers_arrays_strings/1-strcmp.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * _strcmp - Compares two strings
+ *
+ * @s1: Pointer to the first string
+ * @s2: Pointer to the second string
+ *
+ * Return: The difference between the ASCII values of the first non-matching
+ *         character in s1 and s2, or 0 if the strings are equal
+ */
+int _strcmp(char *s1, char *s2)
+{
+int i;
+
+for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+        {
+if (s1[i] != s2[i])
+return (s1[i] - s2[i]);
+}
+
+if (s1[i] == s2[i])
+return (0);
+
+return (s1[i] < s2[i] ? -1 : 1);
+}
