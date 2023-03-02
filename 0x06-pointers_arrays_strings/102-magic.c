7915dad3de65ce6f1740a28ffdac2da9c8539678
Mico Dan <micodan369@gmail.com>
2023-03-02 11:08:22 +0200

    task 10

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,18 +1,24 @@
 #include <stdio.h>
 
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
 int main(void)
 {
-int a[5] = {98, 402, 19876, 1024, 4096};
-int *p;
+  int n;
+  int a[5];
+  int *p;
 
-p = &a[2];
-*(p + 1) = 98;
-printf("a[2] = %d\n", *(p));
-return (0);
+  a[2] = 1024;
+  p = &n;
+  /*
+   * write your line of code here...
+   * Remember:
+   * - you are not allowed to use a
+   * - you are not allowed to modify p
+   * - only one statement
+   * - you are not allowed to code anything else than this line of code
+   */
+  *(p +5) = 98x;
+  /* ...so that this prints 98\n */
+  printf("a[2] = %d\n", a[2]);
+  return (0);
 }
 
