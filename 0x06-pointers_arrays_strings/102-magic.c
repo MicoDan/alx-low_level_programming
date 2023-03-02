887d7d3bb6195eba6f73a12307555608e892a3b0
Mico Dan <micodan369@gmail.com>
2023-03-02 05:52:51 +0200

    done

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -3,19 +3,16 @@
 /**
  * main - Entry point
  *
- * Return: Always 0
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-int a[5] = {0, 1, 2, 3, 4};
+int a[5] = {98, 402, 19876, 1024, 4096};
 int *p;
 
 p = &a[2];
-*p = 98;
-
-/* add your code here */
+*(p + 1) = 98;
 printf("a[2] = %d\n", *(p));
-
 return (0);
 }
+
