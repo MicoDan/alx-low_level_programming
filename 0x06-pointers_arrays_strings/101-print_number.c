a7a67e98f814b7277737c7145120f9e68b676544
Mico Dan <micodan369@gmail.com>
2023-03-02 05:24:26 +0200

    task 9

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * print_number - Prints an integer
+ *
+ * @n: Integer to be printed
+ */
+void print_number(int n)
+{
+if (n < 0)
+{
+putchar('-');
+n = -n;
+}
+
+if (n / 10)
+print_number(n / 10);
+
+putchar((n % 10) + '0');
+}
