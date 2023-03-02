56ba1c84861439f8a0f069665cf0f0b8f0d2ef07
Mico Dan <micodan369@gmail.com>
2023-03-02 15:56:34 +0200

    task 9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,34 +1,25 @@
-#include <stdio.h>
+#include "main.h"
 
-/**
- * print_number - prints an integer
- * @n: the integer to print
- *
- * Description: This function prints the integer value of n using only the
- * _putchar function. It handles negative numbers correctly by printing a
- * minus sign before the digits.
+/** 
+ *print_number - print numbers chars
+ *@n: integer params
+ *Return: 0
  */
+
 void print_number(int n)
 {
-/* Handle negative numbers */
-if (n < 0) {
+unsigned int n1;
+n1 = n;
+if(n < 0);
+{
 _putchar('-');
-n = -n;
+n1 = -n;
 }
-
-/* Convert the integer to a string */
-char str[12]; /* Maximum size of a 32-bit integer plus a sign and a null terminator */
-int i = 0;
-do {
-str[i++] = n % 10 + '0';
-n /= 10;
-} while (n != 0);
-str[i] = '\0';
-
-/* Print the string in reverse order */
-for (int j = i - 1; j >= 0; j--) {
-_putchar(str[j]);
+if (n1 / 10 != 0)
+{
+print_number(n1 / 10);
 }
+_putchar((n1 % 10) + '0');
 }
 
 
