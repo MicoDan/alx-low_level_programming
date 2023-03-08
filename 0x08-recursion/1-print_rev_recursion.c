ebe5669830652286b430a92263d60d8dbc5f90e1
Mico Dan <micodan369@gmail.com>
2023-03-08 17:41:50 +0200

    task 1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * _print_rev_recursion - prints a string in reverse using recursion
+ * @s: string to be printed in reverse
+ *
+ * Return: void
+ */
+void _print_rev_recursion(char *s)
+{
+if (*s != '\0')
+{
+_print_rev_recursion(s + 1);
+putchar(*s);
+}
+}
