c81b122174aba6efff356db293fc40e152ee249d
Mico Dan <micodan369@gmail.com>
2023-03-09 18:14:23 +0200

    alx task5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,17 +1,40 @@
 #include <stdio.h>
 /**
- * _sqrt_recursion - Computes the natural square root of a number
+ * _sqrt_recursion - returns the natural square root of a number
  *
- * @n: The number to compute the square root of
+ * @n: the number to calculate the square root of
  *
- * Return: The natural square root of n, or -1 if n does not have one
+ * Return: the square root of n, or -1 if n doesn't have a natural square root
  */
 int _sqrt_recursion(int n)
 {
 if (n < 0)
-return (-1);
-else if (n == 0 || n == 1)
-return (n);
-else
-return (_sqrt_helper(n, 1));
+{
+return (-1);  /* Return -1 for negative numbers */
 }
+
+if (n == 0 || n == 1)
+{
+return (n);  /* Return n for 0 and 1 */
+}
+
+/* Recursively calculate the square root of n/4 */
+int root = _sqrt_recursion(n / 4);
+
+/* If the square of the calculated root is equal to n, return root */
+if (root * root == n)
+{
+return (root);
+}
+/* If not, check the next odd integer as a possible root */
+else if ((root + 1) * (root + 1) <= n && (root + 1) <= n / (root + 1))
+{
+return (root + 1);
+}
+/* If neither condition is met, n doesn't have a natural square root */
+else
+{
+return (-1);
+}
+}
+
