e292f70894b6367cfa9a0c08d909ef1f5d609ca2
Mico Dan <micodan369@gmail.com>
2023-03-14 08:48:16 +0200

    task of static libraries

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,14 @@
+  GNU nano 6.4                        3-islower.c
+
+
+#include "main.h"
+/**
+ * _islower - checks for lowercase character
+ * @c: the character to check
+ * Return: 1 if c is lowercase, 0 otherwise
+ */
+int _islower(int c)
+{
+return (c >= 'a' && c <= 'z');
+}
+
