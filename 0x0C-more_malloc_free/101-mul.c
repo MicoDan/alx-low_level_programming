84f04bc7be09d6ef5eeccc0b959bf4356a274b7e
Mico Dan <micodan369@gmail.com>
2023-03-16 18:35:32 +0200

    malloc

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,74 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 /**
- * _isdigit - checks if a character is a digit
- * @c: the character to check
- *
- * Return: 1 if the character is a digit, 0 otherwise
+ * is_digit - Check if the string s contains only digits
+ * @s: The string to check
+ * Return: 1 if the string contains only digits, 0 otherwise
  */
-int _isdigit(int c)
+int is_digit(char *s)
 {
-return (c >= '0' && c <= '9');
+while (*s)
+{
+if (!isdigit(*s))
+return (0);
+s++;
+}
+return (1);
 }
 
 /**
- * print_error - prints an error message and exits with a status of 98
+ * main - Entry point. Multiplies two positive numbers
+ * @argc: The number of command line arguments
+ * @argv: An array of strings containing the command line arguments
+ * Return: 0 if success, 98 if arguments are invalid
  */
-void print_error(void)
+int main(int argc, char *argv[])
+{
+int result;
+
+if (argc != 3 || !is_digit(argv[1]) || !is_digit(argv[2]))
 {
 printf("Error\n");
-exit(98);
+return (98);
 }
-
-/**
- * multiply - multiplies two positive numbers
- * @num1: the first number
- * @num2: the second number
- *
- * Return: the product of num1 and num2
- */
-unsigned long int multiply(unsigned int num1, unsigned int num2)
-{
-unsigned long int result = 0;
-while (num2 > 0)
-{
-result += num1 * (num2 % 10);
-num2 /= 10;
-num1 *= 10;
-}
-return (result);
-}
-
-/**
- * main - entry point of the program
- * @argc: the number of command-line arguments
- * @argv: an array of command-line argument strings
- *
- * Return: 0 on success, 98 on failure
- */
-int main(int argc, char **argv)
-{
-unsigned int num1, num2;
-unsigned long int product;
-
-if (argc != 3)
-print_error();
-
-for (int i = 1; i < argc; i++)
-{
-for (int j = 0; argv[i][j] != '\0'; j++)
-{
-if (!_isdigit(argv[i][j]))
-print_error();
-}
-}
-
-num1 = atoi(argv[1]);
-num2 = atoi(argv[2]);
-product = multiply(num1, num2);
-
-printf("%lu\n", product);
-
+result = atoi(argv[1]) * atoi(argv[2]);
+printf("%d\n", result);
 return (0);
 }
