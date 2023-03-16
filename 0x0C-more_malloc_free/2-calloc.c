ea2de3afc2763a277b5bc29f580ad0854b9492be
Mico Dan <micodan369@gmail.com>
2023-03-16 17:58:57 +0200

    malloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+
+/**
+ * _calloc - allocate memory for an array using calloc
+ * @nmemb: the number of elements in the array
+ * @size: the size of each element
+ *
+ * Return: a pointer to the allocated memory, or NULL on failure
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+void *ptr;
+
+/* Check for zero arguments */
+if (nmemb == 0 || size == 0) {
+return (NULL);
+}
+
+/* Allocate memory for the array */
+ptr = calloc(nmemb, size);
+if (ptr == NULL) {
+return (NULL);
+}
+
+/* Zero out the memory */
+memset(ptr, 0, nmemb * size);
+
+return (ptr);
+}
---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+
+/**
+ * array_range - create an array of integers
+ * @min: the minimum value
+ * @max: the maximum value
+ *
+ * Return: a pointer to the newly created array, or NULL on failure
+ */
+int *array_range(int min, int max)
+{
+int *array, i, size;
+
+/* Check for invalid input */
+if (min > max) {
+return (NULL);
+}
+
+/* Calculate the size of the array */
+size = max - min + 1;
+
+/* Allocate memory for the array */
+array = malloc(sizeof(int) * size);
+if (array == NULL) {
+return (NULL);
+}
+
+/* Populate the array with values from min to max */
+for (i = 0; i < size; i++) {
+array[i] = min + i;
+}
+
+return (array);
+}
